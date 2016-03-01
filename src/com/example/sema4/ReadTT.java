adf9b7b172c97f02c6b0131eb062a3f729fc0821
Kieran1701 <kieran1701@comcast.net>
2016-03-01 18:43:17 -0500

    Added code for displaying TT.  Need to write XML for TT still.

---
+++ src/com/example/sema4/ReadTT.java
@@ -0,0 +1,49 @@
+package com.example.sema4;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class ReadTT extends ListActivity {
+		
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       
+        File root = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/train");
+    	   	
+    	File listfile[] = root.listFiles();
+
+    	ArrayList<String> fileList;
+
+    	fileList = new ArrayList<String>();
+    	
+    	if(listfile != null)
+    	{
+	    	for(int i = 0; i < listfile.length; i++)
+	    	{
+	    		fileList.add(listfile[i].getName());
+	    	}
+    	}
+    	ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+    			R.layout.list_item, fileList);
+    	setListAdapter(adapter);
+    }
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		String message = (Environment.getExternalStorageDirectory().getAbsolutePath() + "/train/" + (String) getListAdapter().getItem(position));
+		Intent intent = new Intent(ReadTT.this, ShowTT.class);
+		intent.putExtra("message", message);
+        startActivity(intent);
+	}
+}
--- src/com/example/sema4/TTActivity.java
+++ src/com/example/sema4/TTActivity.java
@@ -1,7 +1,9 @@
 package com.example.sema4;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TTActivity extends Activity {
 
@@ -9,5 +11,27 @@ public class TTActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tt);
+
+        findViewById(R.id.tt_capture_button).setOnClickListener(this);
+        findViewById(R.id.tt_read_button).setOnClickListener(this);
+        findViewById(R.id.tt_save_button).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.tt_capture_button:
+                //TODO
+                break;
+            case R.id.tt_read_button:
+                Intent intent = new Intent(TTActivity.this, ReadTT.class);
+			    startActivity(intent);
+                break;
+            case R.id.tt_save_button:
+                //TODO
+                break;
+            default:
+                break;
+        }
     }
 }
