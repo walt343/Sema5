22020d8337055a811a7e057d118b50c8a7afbec7
Kieran1701 <kieran1701@comcast.net>
2016-03-01 22:30:08 -0500

    ISR List

--- src/com/example/sema4/ISRActivity.java
+++ src/com/example/sema4/ISRActivity.java
@@ -9,5 +9,29 @@ public class ISRActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_isr);
+
+        findViewById(R.id.isr_capture_button).setOnClickListener(this);
+        findViewById(R.id.isr_read_button).setOnClickListener(this);
+        findViewById(R.id.isr_save_button).setOnClickListener(this);
     }
+
+    	public void onClick(View v) {
+		switch(v.getId())
+		{
+			case R.id.acars_capture_button:
+				Toast.makeText(ACARSActivity.this, (String) "TO BE IMPLEMENTED SOON!", Toast.LENGTH_SHORT).show();
+                //TODO
+                
+				break;
+			case R.id.acars_read_button:
+				 Intent intent = new Intent(ISRActivity.this, ReadISR.class);
+			        startActivity(intent);
+				break;
+			case R.id.acars_save_button:
+				Toast.makeText(ACARSActivity.this, (String) "save", Toast.LENGTH_SHORT).show();
+				break;
+			default:
+				break;
+		}
+	}
 }
